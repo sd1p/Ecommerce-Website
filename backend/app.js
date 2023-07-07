1353da870ff475eb9ad878ff3383e478001492a2
Sudip Mandal <skmxyt@gmail.com>
2023-07-07 17:03:17 +0530

    prepared server for deployment

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,20 @@ app.use("/api/v1", productRoutes);
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", orderRoutes);
 
-//Middleware for errors
+//deployment
+const _dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.json({ message: "api working  " });
+  });
+}
 
+//Middleware for errors
 app.use(errorMIddleware);
 
 module.exports = app;
