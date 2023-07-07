8ea289197c992429cde8edd5d6cee6583137e3a9
Sudip Mandal <skmxyt@gmail.com>
2023-07-07 17:50:14 +0530

    updated server.js and tested for production

--- backend/app.js
+++ backend/app.js
@@ -21,19 +21,6 @@ app.use("/api/v1", productRoutes);
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", orderRoutes);
 
-//deployment
-const _dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.json({ message: "api working  " });
-  });
-}
-
 //Middleware for errors
 app.use(errorMIddleware);
 
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 const app = require("./app");
+const express = require("express");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 const cloudinary = require("cloudinary");
+const path = require("path");
 //handling uncaughtException errors
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
   console.log(`Shutting down the server due to Uncaught Error`);
   process.exit(1);
 });
-
+console.log((_dirname = path.resolve(__dirname, "config", "config.env")));
 //config .env variables
-dotenv.config({ path: "backend/config/config.env" });
+dotenv.config({ path: path.resolve(__dirname, "config", "config.env") });
 
 //connecting to db
 connectDatabase();
@@ -21,6 +23,20 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+//deployment
+const _dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname1, "frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.json({ message: "api working  " });
+  });
+}
+console.log(process.env.NODE_ENV);
 const server = app.listen(process.env.PORT, (err) => {
   if (!err) {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
